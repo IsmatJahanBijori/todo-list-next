49a7e989ac2c72a171a51631b24a7f890f7c9961
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-08-19 23:49:04 +0600

    project initialization and home page design added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='font-serif'>
+      <Head>
+      <title>ToDoList</title>
+      </Head>
+      <Navbar/>
+      <Component {...pageProps} />
+      <Footer/>
+    </div>)
 }
